421f08ad7a5da4510b68126642c5d982b9a293cc
unknown <obrienules@gmail.com>
2020-05-05 18:11:37 +0100

    Create and style user dropdown menu

---
+++ components/user.dropdown/user.dropdown.component.js
@@ -0,0 +1,58 @@
+import style from './user.dropdown.module.scss';
+import Links from '../link/link.component';
+
+const signedInLinks = (
+    <>
+        <Links icon={["far","user-circle" ]}pagePath="/">
+                profile
+        </Links>
+        <Links icon={["far", "comments"]} pagePath="/">
+                messages
+        </Links>
+        <Links icon="images" pagePath="/">
+                gallery
+        </Links>
+        <Links icon="business-time" pagePath="/">
+                services
+        </Links>
+        <Links icon={["fas", "star-half-alt"]} pagePath="/">
+                reviews
+        </Links>
+        <Links icon={["fas", "sign-out-alt"]} pagePath="">
+                sign out
+        </Links>
+    </>
+)
+const signedOutLinks = (
+    <>
+        <Links icon="sign-in-alt" pagePath="/">
+            sign in
+        </Links>
+        <Links icon="registered" pagePath="/register">
+            register
+        </Links>
+    </>
+)
+
+const UserDropdown = (props) => {
+    const {isDown, dropdownMenu} = props;
+    const {dropdown, animateDropdown} = style;
+
+    const handleClick = () => {
+        if(typeof dropdownMenu === 'function'){
+            dropdownMenu();
+        }
+     }
+     
+    return (
+        <nav
+        onMouseLeave={handleClick}
+        className={ !isDown ? dropdown : `${dropdown} ${animateDropdown}`}
+         >
+            {signedOutLinks}
+            {signedInLinks}
+        </nav>
+    )
+};
+
+export default UserDropdown
