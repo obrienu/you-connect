7dc79042d638f34a8c09d70c58ceaed235957013
unknown <obrienules@gmail.com>
2020-05-05 01:22:58 +0100

    Initial Setup

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/global.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faCheckSquare, faCoffee)
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+};
