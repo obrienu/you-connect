c7ebdd6e0d2215ddf0ec129286d84add9e1f53d0
unknown <obrienules@gmail.com>
2020-05-05 18:13:48 +0100

    import fontawesome icon to library

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import '../styles/global.scss';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faAddressBook, faRegistered, far } from '@fortawesome/free-regular-svg-icons';
+import {
+    faCheckSquare,
+    faCoffee,
+    faLaptopHouse,
+    faSearch,
+    fas,
+    faBlog,
+    faShoppingCart,
+    faHandsHelping,
+    faSignInAlt,
+    faSignOutAlt,
+    faImages,
 
-library.add(fab, faCheckSquare, faCoffee)
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas, far, faImages, faRegistered, faSignOutAlt,faSignInAlt, faHandsHelping, faCheckSquare, faCoffee, faLaptopHouse, faSearch, faAddressBook, faUser, faBlog, faShoppingCart)
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
