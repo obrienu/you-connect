c83ed024b15070f05747a071251f7eedc1cfb04d
unknown <obrienules@gmail.com>
2020-05-06 19:13:39 +0100

    tweak props and styling

---
+++ components/custom.button/custom.button.component.js
@@ -0,0 +1,18 @@
+import style from './custom.button.module.scss'
+
+const Button = (props) => {
+    const { onClick, type, block, isActive } = props;
+    const handleClick = () => onClick();
+
+    return(
+        <button
+        type={type}
+        className={`${style.button} ${block && style.isBlock } ${isActive && style.isActive }`}
+        onClick={handleClick}
+        >
+            {props.children}
+        </button>
+    )
+};
+
+export default Button
